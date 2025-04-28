413fcc2fc29cf3588cac72c400c11b212981e170
Hudson Newey <neweyh@qut.edu.au>
2025-04-28 10:43:48 +1000

    fix: Hard lock preact and htm cdn versions (#115)
    * fix: Hard lock preact and htm cdn versions
    
    We never used to hard lock preact and htm versions fetched from the cdn,
    meaning that the dependency would automatically update in the production
    environment.
    
    This caused issues when a minor version bump caused the skypack cdn
    builds to fail, causing preact pages to break.
    
    This commit also reduces our attack surface for supply chain attacks.
    
    Fixes: #114
    
    * Fix version lock for preact hooks export

--- assets/js/preact-common.js
+++ assets/js/preact-common.js
@@ -1,20 +1,24 @@
 // https://github.com/preactjs/preact/issues/1961
 //import "https://cdn.skypack.dev/preact/debug";
+//
+// we hard lock preact versions and do not allow minor version bumps because
+// the preact skypack deployment has broken between minor versions before
+// see: https://github.com/ecoacoustics/website/issues/114
 import {
     h,
     Component,
     render,
     createRef,
-} from "https://cdn.skypack.dev/preact";
+} from "https://cdn.skypack.dev/preact@10.26.4";
 import {
     useState,
     useEffect,
     useCallback,
     useReducer,
     useMemo,
-} from "https://cdn.skypack.dev/preact/hooks";
+} from "https://cdn.skypack.dev/preact@10.26.4/hooks";
 
-import htm from "https://cdn.skypack.dev/htm";
+import htm from "https://cdn.skypack.dev/htm@3.1.1";
 
 // Initialize htm with Preact
 const html = htm.bind(h);
